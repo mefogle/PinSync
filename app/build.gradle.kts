f092dc5b59df0eb272248b8d16168070a3b049e2
Mark Fogle <mefogle@gmail.com>
2024-05-18 18:54:10 -0700

    Updating versionCode to 4 and version name to 0.0.2 to match what is in app store.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.pinsync"
         minSdk = 23
         targetSdk = 34
-        versionCode = 3
-        versionName = "0.1"
+        versionCode = 4
+        versionName = "0.0.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
