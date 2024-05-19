639bdbaaa5abe5be45b7f5228e7093287aba965c
Mark Fogle <mefogle@gmail.com>
2024-05-18 18:39:00 -0700

    Updating versionCode to 3.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,7 +12,7 @@ android {
         applicationId = "com.pinsync"
         minSdk = 23
         targetSdk = 34
-        versionCode = 2
+        versionCode = 3
         versionName = "0.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
