fa693c87d30cffc5911ccc1c283306b99ba9da73
Mark Fogle <mefogle@gmail.com>
2024-06-12 19:43:09 -0700

    Upping version.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.pinsync"
         minSdk = 23
         targetSdk = 34
-        versionCode = 4
-        versionName = "0.1.1"
+        versionCode = 6
+        versionName = "0.1.3"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
